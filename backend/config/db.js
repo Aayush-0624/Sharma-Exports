f82abcc53d5c2aaac247f74a6ba2f39b141c5d6b
Aayush-0624 <aayushprivate07@gmail.com>
2026-03-20 23:04:24 +0530

    fix db connection for railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST || process.env.DB_HOST,
-  user: process.env.MYSQLUSER || process.env.DB_USER,
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-  port: process.env.MYSQLPORT || 3306
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 module.exports = db;
