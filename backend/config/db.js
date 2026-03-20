8ef67908bd1263f689f50f35e10376aa2ba41755
Aayush-0624 <aayushprivate07@gmail.com>
2026-03-21 00:53:12 +0530

    fix db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,19 @@
-// config/db.js
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("DB CONNECTION ERROR:", err);
+  } else {
+    console.log("Connected to Railway DB");
+  }
 });
 
 module.exports = db;
